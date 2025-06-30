f387c8761f811bc05c96e127d892ca1b555c1068
Dibya Prokash Sarkar <dibyaprokash@gmail.com>
2025-06-30 21:41:04 +0000

    Change the website's favicon to the following URL: public/favicon_io/fav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,8 +25,11 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/logo/logo.png" type="image/png" sizes="32x32" />
-        <link rel="apple-touch-icon" href="/logo/logo.png" sizes="180x180" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon_io/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon_io/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon_io/favicon-16x16.png" />
+        <link rel="manifest" href="/favicon_io/site.webmanifest" />
+        <link rel="icon" href="/favicon_io/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
