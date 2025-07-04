c01971d96f0d68db2b301ee3490929388285cce6
Dibya Prokash Sarkar <dibyaprokash@gmail.com>
2025-07-04 04:47:21 +0000

    I just noticed that the website's favicon in Google shows perfectly but

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/favicon_io/favicon.ico" />
+        <link rel="icon" type="image/x-icon" href="/favicon_io/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
